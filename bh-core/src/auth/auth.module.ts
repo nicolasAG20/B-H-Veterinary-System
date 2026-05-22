703784b77cef81fa68be0827af47f2cca2dc9c24
Vanessa Toro <vanessatoro906@gmail.com>
2026-05-21 22:06:48 -0500

    feat: Login con JWT, proteccion de endpoints y documentacion

--- bh-core/src/auth/auth.module.ts
+++ bh-core/src/auth/auth.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { Usuario } from '../usuario/entities/usuario.entity';
 import { UsuarioModule } from '../usuario/usuario.module';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    UsuarioModule,
+    PassportModule,
+    TypeOrmModule.forFeature([Usuario]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRATION') as any },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
