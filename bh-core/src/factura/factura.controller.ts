efbd22b2af49b8e28c15a733419accba76970165
Juan Diego Pinilla <juand.pinillaa@autonoma.edu.co>
2026-05-24 17:11:39 -0500

    feat: logica para que la recepcionista aplique descuentos agregada

--- bh-core/src/factura/factura.controller.ts
+++ bh-core/src/factura/factura.controller.ts
@@ -2,10 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@n
 import { FacturaService } from './factura.service';
 import { CreateFacturaDto } from './dto/create-factura.dto';
 import { UpdateFacturaDto } from './dto/update-factura.dto';
+import { AplicarDescuentoDto } from './dto/aplicar-descuento.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolUsuario } from '../usuario/entities/usuario.entity';
 
 @UseGuards(JwtAuthGuard)
-@Controller('facturas')
+@Controller('invoices')
 export class FacturaController {
   constructor(private readonly facturaService: FacturaService) {}
 
@@ -32,5 +36,15 @@ export class FacturaController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.facturaService.remove(+id);
+  }
+
+  @Patch(':id/discount')
+  @UseGuards(RolesGuard)
+  @Roles(RolUsuario.RECEPCIONISTA)
+  aplicarDescuento(
+    @Param('id') id: string,
+    @Body() aplicarDescuentoDto: AplicarDescuentoDto,
+  ) {
+    return this.facturaService.aplicarDescuento(+id, aplicarDescuentoDto);
   }
 }
