45dcb6a69632a8ae9e2c809606692839282c6b8e
Cristian Salazar Arenas <cristiancamilosalazararenas@outlook.com>
2026-05-25 04:23:49 -0500

    feat: Se creó el proyecto vacío para la trazabilidad con el application.properties configurado

---
+++ bh-audit/src/test/java/com/bh/audit/BhAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bh.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
